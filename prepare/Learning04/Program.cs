f9d228074cd1ff42ba3ed9124cf62a757a1ac5fd
a-barret <aidenb360@>
2025-02-20 23:39:45 -0700

    Week 7 learning activity

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment a = new Assignment("Aiden Barrett", "Multiplication");
+        Console.WriteLine(a.GetSummary()); 
+
+        MathAssignment ma = new MathAssignment("Aiden Barrett", "Multiplication", "7.3", "8-19");
+        Console.WriteLine(ma.GetSummary()); 
+        Console.WriteLine(ma.GetHomeworkList());
+
+        WritingAssignment wa = new WritingAssignment("Aiden Barrett", "Multiplication", "The Causes of World War II");
+        Console.WriteLine(wa.GetSummary()); 
+        Console.WriteLine(wa.GetWritingInformation());
     }
 }
