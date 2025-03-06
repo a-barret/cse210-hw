8d3bf59c683e9fe29ac1fa2eda88d774776b5c88
a-barret <aidenb360@>
2025-03-06 01:44:11 -0700

    Polymorphism Learning Activity Complete

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square s = new Square("Green", 5);
+        Console.WriteLine(s.GetColor());
+        Console.WriteLine(s.GetArea());
+
+        Circle c = new Circle("Blue", 3);
+        Console.WriteLine(c.GetColor());
+        Console.WriteLine(c.GetArea());
+
+        Rectangle r = new Rectangle("Yellow", 3, 5);
+        Console.WriteLine(r.GetColor());
+        Console.WriteLine(r.GetArea());
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(s);
+        shapes.Add(c);
+        shapes.Add(r);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+        }
     }
 }
